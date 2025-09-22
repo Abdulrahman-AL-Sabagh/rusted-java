d970755f7ea32db05468b6ef13c1b8f950e69023
Abdulrahamn-AL-Sabagh <abdsabagh8@gmail.com>
2025-09-23 00:06:40 +0200

    map sring to keywordtoken

--- src/Token.rs
+++ src/Token.rs
@@ -1,3 +1,18 @@
+struct Span {
+    col_start: usize,
+    col_end: usize,
+    line_start: usize,
+    line_end: usize
+
+}
+
+struct Token {
+    kind: TokenKind,
+    position: Span,
+    value: String
+}
+
+
 pub enum TokenKind {
     Identifier,
     Keyword(KeywordToken)
@@ -18,11 +33,39 @@ pub enum KeywordToken {
     While,
     IF,
     Else,
+    Void,
 }
+
+
+impl KeywordToken {
+
+    fn map_string_to_token(value: &str) -> Result<TokenKind, String> {
+        return match(value) {
+           
+            "new" => TokenKind::Keyword(KeywordToken::New),
+            "class" => TokenKind::Keyword(KeywordToken::Class),
+            "char" => TokenKind::Keyword(KeywordToken::Char),
+            "int" => TokenKind::Keyword(KeywordToken::Int),
+            "print" => TokenKind::Keyword(KeywordToken::Print),
+            "break" => TokenKind::Keyword(KeywordToken::Break),
+            "continue" => TokenKind::Keyword(KeywordToken::Continue),
+            "while" => TokenKind::Keyword(KeywordToken::While),
+            "if" => TokenKind::Keyword(KeywordToken::If),
+            "else" => TokenKind::Keyword(KeywordToken::Else),
+            "void" => TokenKind::Keyword(KeywordToken::Void),
+            _ => Err(format!("Unexpected keywordtoken:  {}", value)),
+        }
+    
+    }
+
+}
+
+
+
 
 pub enum SpecialChar {
     Slash,
-    
+
 } 
 
 pub enum TypeKind {
